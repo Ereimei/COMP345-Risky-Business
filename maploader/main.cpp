400dad2e68ffc42f68b29a8c0787295c6388fe17
Patrick Bednarski <patrick.bednarski@yuvo.io>
2017-09-20 17:55:15 -0400

    Scaffold the parts

---
+++ maploader/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Patrick Bednarski
+ *
+ * Created on September 19, 2017, 3:40 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using std::cout;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "If you guys can run this then you've setup correctly!";
+    return 0;
+}
+
