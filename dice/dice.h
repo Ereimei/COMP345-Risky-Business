e83efed68adea62e213febc207ee991207a87473
Jamesff <23jamesff@gmail.com>
2017-09-25 23:43:51 -0400

    adding diepool class and driver
    contains the dice roller class, that contains a dice roll function and stat tracker

---
+++ dice/dice.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   dice.h
+ * Author: eagle
+ *
+ * Created on September 25, 2017, 9:20 PM
+ */
+
+#ifndef DICE_H
+#define DICE_H
+
+
+
+#endif /* DICE_H */
+
