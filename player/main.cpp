a30697c9f11569fd15fc30be248ff9b70ac09eae
OlivierTD <oli.trep@gmail.com>
2017-09-27 15:43:26 -0400

    Created driver for part 4. Still need to modify it a little bit.

--- player/main.cpp
+++ player/main.cpp
@@ -1,20 +1,43 @@
 /* 
  * File:   main.cpp
- * Author: Patrick Bednarski
+ *  *** AUTHORS ***
+ * Patrick Bednarski, 40002239
+ * Hiu Tung Lam (Emily), 26311326
+ * James Ferreira-Fernandez, 21964216
+ * Olivier Trepanier-Desfosses, 27850956
+ *	
+ *** COMP 345 SECTION D ***
+ * Assignment #1
+ * Professor: Dr. Joey Paquet
  *
- * Created on September 19, 2017, 3:40 PM
+ * Created on September 27, 2017, 3:40 PM
  */
 
 #include <cstdlib>
 #include <iostream>
+#include "Player.h"
 
-using std::cout;
+using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-    cout << "If you guys can run this then you've setup correctly!";
-    return 0;
+    
+    cout << "Creating player object." << endl;
+    Player demoplayer;
+    
+    cout << endl << "Player owns a hand of Risk cards" << endl;
+    cout << demoplayer.getHand() << endl;
+    
+    cout << endl << "Player has his own dice rolling facility object" << endl;
+    cout << demoplayer.getDiepool() << endl;
+    
+    cout << endl << "Player has his own territories" << endl;
+    cout << demoplayer.getTerritories() << endl;
+    
+    cout << "Player's functions" << endl << "-------------------" << endl;
+    cout << demoplayer.attack() << endl;
+    cout << demoplayer.fortify() << endl;
+    cout << demoplayer.reinforce() << endl;
+    
+    
 }
 
