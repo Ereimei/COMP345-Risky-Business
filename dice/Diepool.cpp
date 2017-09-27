b96919ca9ac1dff57b4e763c83bd4440bc290df4
eagle <23jamesff@gmail.com>
2017-09-27 12:13:54 -0400

    Separated the Diepool from the main, and cleaned up the code somewhat.

---
+++ dice/Diepool.cpp
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include "dice.h"
+
+using std::cout;
+
+   Diepool::Diepool(){
+        num1 = 0;
+        num2 = 0;
+        num3 = 0;
+        num4 = 0;
+        num5 = 0;
+        num6 = 0;
+        numt = 0;
+        dice[0] = 1;
+        dice[1] = 1;
+        dice[2] = 1;
+    }
+    
+    void Diepool::roll(){
+        srand((int)time(NULL));
+        int numDice = 0;
+        std::cout << "Enter number of dice to be used(1,2 or 3)" << std::endl;
+        std::cin >> numDice;
+       
+        while(numDice < 1 || numDice > 3){
+            std::cout << "Incorrect value. Please enter a 1,2 or 3." << std::endl;
+            std::cin >> numDice;
+        }
+        for(int i = 0; i <= numDice - 1; i++){
+            this->dice[i] = (rand() % 6) + 1;
+            std::cout << "you rolled a " << dice[i] << std::endl;
+
+            this->numt++;
+            switch(dice[i]){
+                case 1: 
+                    this->num1++;
+                    break;
+                case 2:
+                    this->num2++;
+                    break;
+                case 3:
+                    this->num3++;
+                    break;
+                case 4:
+                    this->num4++;
+                    break;
+                case 5:
+                    this->num5++;
+                    break;
+                case 6:
+                    this->num6++;
+                    break;
+            } //end of switch case
+        }//end of for loop 
+    }//end of function
+    
+    void Diepool::showPercentage(){
+        std::cout << "You have rolled 1 " << (100 * this->num1 / this->numt) << " % of the time" << std::endl;
+        std::cout << "You have rolled 2 " << (100 * this->num2 / this->numt) << " % of the time" << std::endl;
+        std::cout << "You have rolled 3 " << (100 * this->num3 / this->numt) << " % of the time" << std::endl;
+        std::cout << "You have rolled 4 " << (100 * this->num4 / this->numt) << " % of the time" << std::endl;
+        std::cout << "You have rolled 5 " << (100 * this->num5 / this->numt) << " % of the time" << std::endl;
+        std::cout << "You have rolled 6 " << (100 * this->num6 / this->numt) << " % of the time" << std::endl;
+    }
--- dice/main.cpp
+++ dice/main.cpp
@@ -1,20 +1,38 @@
 /* 
  * File:   main.cpp
- * Author: Patrick Bednarski
+ * Author: Patrick Bednarski, 40002239
+ * Emily Lam, 26311326
+ * James Ferreira-Fernandez, 21964216
+ * Olivier Trepanier-Desfosses, 27850956
  *
  * Created on September 19, 2017, 3:40 PM
  */
 
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
+#include "dice.h"
 
 using std::cout;
+
+ 
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    cout << "If you guys can run this then you've setup correctly!";
+    Diepool dice1;
+    dice1.roll();
+    dice1.roll();
+    dice1.roll();
+    dice1.roll();
+    dice1.showPercentage();
+    Diepool dice2;
+    dice2.roll();
+    dice2.roll();
+    dice2.roll();
+    dice2.showPercentage();
+    
     return 0;
 }
 
