24c845a081ffc48371b24181700f1bbdb6d0add6
OlivierTD <oli.trep@gmail.com>
2017-09-27 15:43:01 -0400

    Added header comment for files

--- player/Player.cpp
+++ player/Player.cpp
@@ -1,20 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Player.cpp
- * Author: olitr
- * 
- * Created on September 27, 2017, 9:38 AM
+ *  *** AUTHORS ***
+ * Patrick Bednarski, 40002239
+ * Hiu Tung Lam (Emily), 26311326
+ * James Ferreira-Fernandez, 21964216
+ * Olivier Trepanier-Desfosses, 27850956
+ *	
+ *** COMP 345 SECTION D ***
+ * Assignment #1
+ * Professor: Dr. Joey Paquet
+ *
+ * Created on September 27, 2017, 3:40 PM
  */
 
 #include "Player.h"
 using namespace std
-
-Player::Player(vector<Territory*> territories, Hand hand, Diepool diepool) : territories(territories),
+        
+//Constructor
+Player::Player(vector<Territory> territories, Hand hand, Diepool diepool) : territories(territories),
     hand(hand),
     diepool(diepool) {}
 
@@ -22,12 +25,20 @@ Player::Player(const Player& orig) : territories(orig.getTerritories()),
         hand(orig.getHand()),
         diepool(orig.getDiepool()) {}
 
+//Destructors
 Player::~Player() {}
 
-vector<Territory*> Player::getTerritories() {return territories;}
+//Getters
+vector<Territory> Player::getTerritories() {return territories;}
 Hand Player::getHand() {return hand;}
 Diepool Player::getDiepool() {return diepool;}
 
+//Setters
+Player::setTerritories(vector<Territory>) {this->territories = vector<Territory>;}
+Player::setHand(Hand hand) {this->hand = hand;}
+Player::setDiePool(Diepool diepool) {this->diepool = diepool;}
+
+//Other functions
 Player::reinforce(){ cout << "This player can reinforce!" << endl;}
 Player::attack(){ cout << "This player can attack!" << endl;}
 Player::fortify(){ cout << "This player can fortify" << endl;}
--- player/Player.h
+++ player/Player.h
@@ -1,16 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Player.h
- * Author: olitr
+ *  *** AUTHORS ***
+ * Patrick Bednarski, 40002239
+ * Hiu Tung Lam (Emily), 26311326
+ * James Ferreira-Fernandez, 21964216
+ * Olivier Trepanier-Desfosses, 27850956
+ *	
+ *** COMP 345 SECTION D ***
+ * Assignment #1
+ * Professor: Dr. Joey Paquet
  *
- * Created on September 27, 2017, 9:38 AM
+ * Created on September 27, 2017, 3:40 PM
  */
-
 
 #ifndef PLAYER_H
 #define PLAYER_H
