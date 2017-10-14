83e6e41aa241ec8127a35b0ad729474873c648ff
Patrick Bednarski <patrick.bednarski@yuvo.io>
2017-10-14 13:45:14 -0400

    Create gamestarter project

---
+++ gamestarter/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Patman1
+ *
+ * Created on October 14, 2017, 1:36 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Running game starter" << endl;
+    return 0;
+}
+
