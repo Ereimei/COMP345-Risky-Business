198d39b83cd4e932c5a485cc5adb20cd704971a2
LAPTOP-C4SUSU3Q\Emily Lam <e.mily.lam@hotmail.com>
2017-10-15 23:30:23 -0400

    - created project file called "startup" for part 2 of assignment 2

---
+++ startup/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+
+File: main.cpp
+ *** AUTHORS ***
+
+Patrick Bednarski, 40002239
+Hiu Tung Lam (Emily), 26311326
+James Ferreira-Fernandez, 21964216
+Olivier Trepanier-Desfosses, 27850956
+ *** COMP 345 SECTION D ***
+
+Assignment # 2
+Professor: Dr. Joey Paquet
+Created on October 15, 2017, 11:25 PM */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "hello" << endl;
+    return 0;
+}
+
