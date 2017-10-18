c32fc5a0f17e724208bb3722ac8db95b8d86ea1e
LAPTOP-C4SUSU3Q\Emily Lam <e.mily.lam@hotmail.com>
2017-10-18 19:57:12 -0400

    no message

---
+++ startup/startup.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+
+File: startup.cpp
+ *** AUTHORS ***
+
+Patrick Bednarski, 40002239
+Hiu Tung Lam (Emily), 26311326
+James Ferreira-Fernandez, 21964216
+Olivier Trepanier-Desfosses, 27850956
+ *** COMP 345 SECTION D ***
+
+Assignment # 2
+Professor: Dr. Joey Paquet
+Created on October 17, 2017, 10:20 PM */
+
+# include "startup.h"
