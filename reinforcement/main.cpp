8e87fb8d0e4544412f7c4f6ad903374c20fe5435
OlivierTD <oli.trep@gmail.com>
2017-10-21 19:32:52 -0400

    Created new reinforcement project.

---
+++ reinforcement/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+
+#include "../player/Player.h"
+
+using namespace std;
+
+int numArmies(Player);
+
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+
+int numArmies(Player){
+    
+    
+    
+    
+}
