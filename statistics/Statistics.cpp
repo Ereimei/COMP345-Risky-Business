de4cc9e42111f617d0d6f67997fd264fcde5e917
Patrick Bednarski <patrick.bednarski@yuvo.io>
2017-11-04 23:17:59 -0400

    Create statistics classes

---
+++ statistics/Statistics.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   Statistics.cpp
+ * Patrick Bednarski, 40002239
+ * Hiu Tung Lam (Emily), 26311326
+ * James Ferreira-Fernandez, 21964216
+ * Olivier Trepanier-Desfosses, 27850956
+ * 
+ *** COMP 345 SECTION D ***
+ * Assignment #
+ * Professor: Dr. Joey Paquet
+ * 
+ * Created on November 4, 2017, 11:13 PM
+ */
+
+#include "Statistics.h"
+
+Statistics::Statistics() {
+}
+
+Statistics::Statistics(const Statistics& orig) {
+}
+
+Statistics::~Statistics() {
+}
+
---
+++ statistics/Statistics.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   Statistics.h
+ * Patrick Bednarski, 40002239
+ * Hiu Tung Lam (Emily), 26311326
+ * James Ferreira-Fernandez, 21964216
+ * Olivier Trepanier-Desfosses, 27850956
+ * 
+ *** COMP 345 SECTION D ***
+ * Assignment #3
+ * Professor: Dr. Joey Paquet
+ *
+ * Created on November 4, 2017, 11:13 PM
+ */
+
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+class Statistics {
+public:
+    Statistics();
+    Statistics(const Statistics& orig);
+    virtual ~Statistics();
+private:
+
+};
+
+#endif /* STATISTICS_H */
+
