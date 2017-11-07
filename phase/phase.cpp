2f9b4be11c270d415b4c14062beee7892a8e2241
LAPTOP-C4SUSU3Q\Emily Lam <e.mily.lam@hotmail.com>
2017-11-07 12:14:54 -0500

    - phase project

---
+++ phase/phase.cpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+
+File: phase.cpp
+ *** AUTHORS ***
+
+Patrick Bednarski, 40002239
+Hiu Tung Lam (Emily), 26311326
+James Ferreira-Fernandez, 21964216
+Olivier Trepanier-Desfosses, 27850956
+ *** COMP 345 SECTION D ***
+
+Assignment #
+Professor: Dr. Joey Paquet
+Created on November 7, 2017, 12:07 PM */
